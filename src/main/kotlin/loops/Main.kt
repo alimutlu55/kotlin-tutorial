db5bf4e1cd57ffab86e165af990cdb9f1540297b
alimutlu <alimutlu_5529@icloud.com>
2022-10-28 20:51:17 +0200

    loop implementations added.

---
+++ src/main/kotlin/loops/Main.kt
@@ -0,0 +1,53 @@
+package loops
+
+
+val fruits = listOf("Apple", "Banana", "Strawberry", "Orange")
+
+fun main() {
+    // dots()
+    // until()
+    // lastIndex()
+    // indices()
+    // withIndex()
+    forEachIndexed()
+}
+
+fun forEachIndexed() {
+    fruits.forEachIndexed { index, fruit ->
+        println("$index : $fruit")
+    }
+}
+
+fun withIndex() {
+    for ((index, fruit) in fruits.withIndex()) {
+        println("$index : $fruit")
+    }
+}
+
+fun indices() {
+    for (index in fruits.indices) {
+        val fruit = fruits[index]
+        println("$index : $fruit")
+    }
+}
+
+fun lastIndex() {
+    for (index in 0..fruits.lastIndex) {
+        val fruit = fruits[index]
+        println("$index : $fruit")
+    }
+}
+
+fun dots() {
+    for (index in 0..fruits.size - 1) {
+        val fruit = fruits[index]
+        println("$index : $fruit")
+    }
+}
+
+fun until() {
+    for (index in 0 until fruits.size) {
+        val fruit = fruits[index]
+        println("$index : $fruit")
+    }
+}
