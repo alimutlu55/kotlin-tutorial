91141b41c23f5893847a2117193314e0782cf068
alimutlu <alimutlu_5529@icloud.com>
2022-10-31 22:27:55 +0100

    dispatchers are added.

--- src/main/kotlin/coroutines/StructuredConcurrency.kt
+++ src/main/kotlin/coroutines/StructuredConcurrency.kt
@@ -4,6 +4,14 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.runBlocking
 
+/*
+In a real application, you will be launching a lot of coroutines.
+Structured concurrency ensures that they are not lost and do not leak.
+An outer scope cannot complete until all its children coroutines complete.
+Structured concurrency also ensures that any errors in the code are properly reported and are never lost.
+ */
+
+
 fun main() = runBlocking {
     println(concurrentSum())
 }
--- src/main/kotlin/coroutines/StructuredConcurreny.kt
+++
@@ -1,9 +0,0 @@
-package coroutines
-
-/*
-
-In a real application, you will be launching a lot of coroutines.
-Structured concurrency ensures that they are not lost and do not leak.
-An outer scope cannot complete until all its children coroutines complete.
-Structured concurrency also ensures that any errors in the code are properly reported and are never lost.
- */
