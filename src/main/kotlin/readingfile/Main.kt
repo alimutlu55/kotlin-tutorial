f6e98c1270a870112eb768085a139723157bbaa0
alimutlu <alimutlu_5529@icloud.com>
2022-11-09 22:21:42 +0100

    read file functions are added.

---
+++ src/main/kotlin/readingfile/Main.kt
@@ -0,0 +1,23 @@
+package readingfile
+
+import java.io.File
+
+fun readFileLineByLineUsingForEachLine(fileName: String)
+        = File(fileName).forEachLine { println(it) }
+
+
+fun readFileAsLinesUsingUseLines(fileName: String): List<String>
+        = File(fileName).useLines { it.toList() }
+
+fun readFileAsLinesUsingBufferedReader(fileName: String): List<String>
+        = File(fileName).bufferedReader().readLines()
+
+fun readFileAsLinesUsingReadLines(fileName: String): List<String>
+        = File(fileName).readLines()
+
+fun readFileAsTextUsingInputStream(fileName: String)
+        = File(fileName).inputStream().readBytes().toString(Charsets.UTF_8)
+
+fun readFileDirectlyAsText(fileName: String): String
+        = File(fileName).readText(Charsets.UTF_8)
+
